2207a6b22088cc15fdfc2519e190e12c942b3b89
David Carpenter <davidc538@hotmail.com>
2016-10-29 17:13:55 -0400

    split files

---
+++ functors.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "puzzle_state.hpp"
+
+struct hybrid_comparator
+{
+	bool operator() (const puzzle_state_search& l, const puzzle_state_search& r)
+	{
+		int l_cor = l.current.correctly_placed_tiles(),
+			r_cor = r.current.correctly_placed_tiles(),
+			l_man = l.current.manhattan_distance(),
+			r_man = r.current.manhattan_distance(),
+			l_steps = l.steps_taken,
+			r_steps = r.steps_taken,
+			l_h = (0 - l_cor + l_man + l_steps),
+			r_h = (0 - r_cor + r_man + r_steps);
+
+		return (l_h > r_h);
+	}
+};
+
+struct distance_comparator
+{
+	bool operator() (const puzzle_state_search& l, const puzzle_state_search& r)
+	{
+		return ((l.current.manhattan_distance() + l.steps_taken)
+			> (r.current.manhattan_distance() + r.steps_taken));
+	}
+};
+
+struct distance_hash_comparator
+{
+	bool operator() (const puzzle_state_search& l, const puzzle_state_search& r)
+	{
+		int l_heur = l.current.manhattan_distance() + l.steps_taken;
+		int r_heur = r.current.manhattan_distance() + r.steps_taken;
+
+		if (l_heur != r_heur)
+			return (l_heur > r_heur);
+		else
+			return (l.current.hash() > r.current.hash());
+	}
+};
+
+struct hash_comparator
+{
+	bool operator() (const puzzle_state& l, const puzzle_state& r) const
+	{
+		return (l.hash() > r.hash());
+	}
+};
